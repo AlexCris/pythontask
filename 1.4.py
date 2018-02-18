c42d74e6c7f6316f30b6c3b84f5d390715f83ea4
AlexCris <35621286+AlexCris@users.noreply.github.com>
2018-02-18 12:16:21 +0300

    Create 1.4.py

---
+++ 1.4.py
@@ -0,0 +1,56 @@
+# Вариант 1
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print(number1, "+", number2, "=", number1+number2)
+# Вариант 2
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "-", number2, ")**3", "=", (number1-number2)**3)
+# Вариант 3
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print(number1, "*", number2, "=", number1*number2)
+# Вариант 4
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print(number1, "**", number2, "=", number1**number2)
+# Вариант 5
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "+", number2, ")**2", "=", (number1+number2)**2)
+# Вариант 6
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "-", number2, ")**2", "=", (number1-number2)**2)
+# Вариант 7
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "*", number2, ")**2", "=", (number1*number2)**2)
+# Вариант 8
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print(number1, "-", number2, "=", number1-number2)
+# Вариант 9
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "/", number2, ")**2", "=", (number1/number2)**2)
+# Вариант 10
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "+", number2, ")**3", "=", (number1+number2)**3)
+# Вариант 11
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "/", number2, ")**3", "=", (number1/number2)**3)
+# Вариант 12
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print("(", number1, "*", number2, ")**3", "=", (number1*number2)**3)
+# Вариант 13
+number1 = int(input("Первое число: "))
+number2 = int(input("Второе число: "))
+print(number1, "/", number2, "=", number1/number2)
+# Вариант 14
+e = 2,718281
+number = int(input("Второе число: "))
+print(e, "**", number, "=", e**number)
